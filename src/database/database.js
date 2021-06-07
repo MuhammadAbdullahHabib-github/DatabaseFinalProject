40a1118e3fec58fc8e97f32802094d83199cd3a7
Muhammad Abdullah Habib <u2019274@giki.edu.pk>
2021-06-07 07:11:01 +0500

    script editing

--- src/database/database.js
+++ src/database/database.js
@@ -2,13 +2,20 @@ const Pool = require('pg').Pool;
 const dotenv = require('dotenv');
 dotenv.config();
 
-const {Host , Database , User , Password} = process.env;
+// const devConfig = {
+//     host:process.env.Host,
+//     database:process.env.Database,
+//     user:process.env.User,
+//     password:process.env.Password
+// };
+
+const devConfig = `postgresql://${process.env.User}:${process.env.Password}@${process.env.Host}/${process.env.Database}`;
+
+const prodConfig = process.env.DATABASE_URL;
+
 
 const database = new Pool({
-    host:Host,
-    database:Database,
-    user:User,
-    password:Password
-})
+    connectionString:process.env.NODE_ENV === "production" ? prodConfig : devConfig
+});
 
 module.exports = database;
--- src/server.js
+++ src/server.js
@@ -3,6 +3,7 @@ const app = express();
 const cors = require('cors');
 const dotenv = require('dotenv');
 const cookieParser = require("cookie-parser");
+const path = require("path")
 dotenv.config();
 
 //port
@@ -12,7 +13,13 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 app.use(cookieParser());
+app.use(express.static(path.join(__dirname , ("../client/build"))));
 
+if(process.env.NODE_ENV === "production"){
+    app.use(express.static(path.join(__dirname , ("../client/build"))))
+}
+
+console.log(path.join(__dirname , ("../client/build")))
 //Student Routes
 app.use("/api/student/" , require('./Routes/students'));
 app.use("/api/dashboard" , require('./Routes/dashboard'));
